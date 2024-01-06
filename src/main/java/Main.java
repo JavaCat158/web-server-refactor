500941ad69de2eea2c689b88632f1aa82580d1cf
Ildar <ibragimovildar229@gmail.com>
2024-01-06 17:28:14 +0300

    final-constructor-fault

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,7 @@
-import java.io.BufferedOutputStream;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.Arrays;
-import java.util.Map;
+
 
 public class Main {
     private static final int SERVER_PORT = 9999;
--- src/main/java/Request.java
+++ src/main/java/Request.java
@@ -1,20 +1,20 @@
 import org.apache.http.NameValuePair;
 import org.apache.http.client.utils.URLEncodedUtils;
-
 import java.nio.charset.StandardCharsets;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
+
 
 public class Request {
 
     private final String method;
     private final String path;
+    private final List<NameValuePair> queryParams;
 
 
     public Request(String requestMethod, String requestPath) {
         this.method = requestMethod;
         this.path = requestPath;
+        this.queryParams = getQueryParam(requestPath);
     }
 
     public String getMethod() {
@@ -24,22 +24,12 @@ public class Request {
     public String getPath() {
         return path;
     }
-    public String getQueryParam(String name) {
-        List<NameValuePair> params = URLEncodedUtils.parse(this.getPath(), StandardCharsets.UTF_8);
-        for(NameValuePair param: params) {
-            if (param.getName().equals(name)) {
-                return param.getValue();
-            }
-        }
-        return null;
+
+    private List<NameValuePair> getQueryParam(String name) {
+        return URLEncodedUtils.parse(name, StandardCharsets.UTF_8);
     }
 
-    public Map<String, String> getQueryParams() {
-        List<NameValuePair> params = URLEncodedUtils.parse(this.getPath(), StandardCharsets.UTF_8);
-        Map<String, String> queryParams = new HashMap<>();
-        for(NameValuePair param : params) {
-            queryParams.put(param.getName(), param.getValue());
-        }
+    public List<NameValuePair> getQueryParams() {
         return queryParams;
     }
 }
